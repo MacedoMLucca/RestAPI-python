180185860a56213e82bce59c7efc11815145c91d
Lucca Macedo <luccae.macedo@gmail.com>
2024-04-08 17:48:25 -0300

    Criando Banco de Dados, tabela e registro teste

---
+++ cria_banco.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+connection = sqlite3.connect('banco.db')
+cursor = connection.cursor()
+
+cria_tabela = "CREATE TABLE IF NOT EXISTS hoteis(hotel_id text PRIMARY KEY,\
+                                                nome text, diaria real, cidade text)"
+atualiza_tabela = "ALTER TABLE hoteis ADD estrelas real AFTER nome"
+
+cria_hotel = "INSERT INTO hoteis VALUES ('alpha','alpha Hotel', 4.3, 345.30,'Rio de janeiro')"
+
+cursor.execute(cria_tabela)
+
+cursor.execute(atualiza_tabela)
+cursor.execute(cria_hotel)
+
+connection.commit()
+connection.close()
